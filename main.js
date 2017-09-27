f681be289436ae8c2d9b56f4bdd9215274a37b82
Noah Teshima <noah.teshima@gmail.com>
2017-09-27 01:36:37 -0700

    separated search results and bar, added jQuery anonymous call on load

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(init);
+
+function init() {
+	var cx = "001892190707907696787:e20tcxfmvrc";
+	var gcse = document.createElement("script");
+	gcse.type = "text/javascript";
+	gcse.async = true;
+	gcse.src = "https://cse.google.com/cse.js?cx=" + cx;
+	var s = document.getElementsByTagName("script")[0];
+	s.parentNode.insertBefore(gcse, s);
+}
