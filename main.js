67abda3a830372abb938705442ca6b441c844c17
Noah Teshima <noah.teshima@gmail.com>
2017-09-29 09:35:47 -0700

    added asynchronous request for search results in JSON format

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-$(document).ready(init);
+//$(document).ready(init);
 
-function init() {
+
+(function init() {
 	var cx = "001892190707907696787:e20tcxfmvrc";
 	var gcse = document.createElement("script");
 	gcse.type = "text/javascript";
@@ -8,4 +9,22 @@ function init() {
 	gcse.src = "https://cse.google.com/cse.js?cx=" + cx;
 	var s = document.getElementsByTagName("script")[0];
 	s.parentNode.insertBefore(gcse, s);
+
+	var key = "AIzaSyCW_bq0dqdR5pGuA5irKBJG9dAPjxnIzz4";
+
+	$("#childOne").on("keyup", function(event) {
+		if(event.which === 13) {
+			asyncGETRequest("https://www.googleapis.com/customsearch/v1?key=" + key + "&cx=" + cx + "&q=lectures");
+		}
+	})
+}());
+function asyncGETRequest(query) {
+	var request = new XMLHttpRequest();
+	request.addEventListener("load", callback);
+	request.open("GET", query);
+	request.setRequestHeader("Content-Type", "application/json");
+	request.send();
+	function callback() {
+		console.log(JSON.parse(this.responseText));
+	}
 }
